14fa2d9f56264f497bded061518c9f3f6ecb4ad1
tomomichi <tomomichi.onishi@gmail.com>
2019-09-11 00:35:59 +0900

    :+1: WIP: Add csv for search index

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,30 @@
-const functions = require('firebase-functions');
+// const functions = require('firebase-functions');
+const fs = require('fs');
+const csv = require('csv-parse');
 
-// Books
-exports.books = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// // Books
+// exports.books = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+//
+// // Persons
+// exports.persons = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
 
-// Persons
-exports.persons = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+
+// const results = [];
+// fs.createReadStream('csv/persons.csv')
+//   .pipe(csv())
+//   .on('data', (data) => { if(data[1].match(/芥川/)){ results.push(data) } })
+//   .on('end', () => {
+//     console.log(results);
+//   });
+
+const results = [];
+fs.createReadStream('csv/books.csv')
+  .pipe(csv())
+  .on('data', (data) => { if(data[1].match(/食/)){ results.push(data[0]) } })
+  .on('end', () => {
+    console.log(results);
+  });
