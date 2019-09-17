088fe7b103a4a6cc301519845ab3edbc9e4e42b9
tomomichi <tomomichi.onishi@gmail.com>
2019-09-17 17:06:56 +0900

    update books search

--- functions/index.js
+++ functions/index.js
@@ -6,25 +6,24 @@ admin.initializeApp();
 
 // Books
 exports.books = functions.https.onRequest((request, response) => {
-  let results = [];
+  let results = { books: [], links: {} };
   let docRef = admin.firestore().collection('books');
 
   // 正規表現があるとき（他のクエリは無視）
   let bookRegex = (request.query['作品名'] || "").match(/\/(.*)\//);
-  let personRegex = (request.query['姓名'] || "").match(/\/(.*)\//);
-  if(bookRegex || personRegex ) {
-    let br = bookRegex ? bookRegex[1] : "";
-    let pr = personRegex ? personRegex[1] : "";
-    let matchedIDs = doSearch(br, pr).then(function(matchedIDs){
+  if(bookRegex) {
+    let matchedIDs = doSearch(bookRegex[1], request.query['人物ID']).then(function(matchedIDs){
       // 正規表現時のページング処理
       let startIndex = (after = request.query['after']) ? matchedIDs.findIndex(after) : -1;
       let limit = Math.min(request.query['limit'] || 50, 50);
       matchedIDs = matchedIDs.slice(startIndex + 1, startIndex + limit);
 
+      // TODO: next/prevリンク組み立て
+
       let docRefs = matchedIDs.map(function(m){ return docRef.doc(m); });
       admin.firestore().getAll(...docRefs).then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-          results.push(doc.data());
+          results["books"].push(doc.data());
         });
         response.status(200).send(results);
       })
@@ -35,25 +34,24 @@ exports.books = functions.https.onRequest((request, response) => {
   // それ以外はrequestから検索クエリ組み立て
   } else {
     for(let [key, val] of Object.entries(request.query)) {
-      if(!Array.isArray(val)) { val = new Array(val); } // 同一カラムに複数クエリのケースがあるので、valは配列に揃える
-      val.forEach(v => {
-        let match = v.match(/([=<>]*)(.*)/);
-        let operator = (match[1]=="") ? '==' : match[1];
-        docRef = docRef.where(key, operator, match[2]);
-      })
+      if(["order", "limit", "after", "before"].include(key)) { continue; }
+      docRef = docRef.where(key, "==", val);
     }
-    // order, after, limit
+    // order, after/before, limit
     if(order = request.query['order']) {
-      let match = order.match(/([⌃˅]?)(.*)/);
-      docRef = (match[1]=="˅") ? docRef.orderBy(match[2], 'desc') : docRef.orderBy(match[2])
+      let match = order.match(/(.*)([⌃˅]?)/);
+      docRef = (match[2]=="⌃") ? docRef.orderBy(match[1]) : docRef.orderBy(match[1], 'desc');
     }
     if(after = request.query['after']) { docRef = docRef.startAfter(after); }
+    if(before = request.query['before']) { docRef = docRef.endBefore(before); }
     let limit = Math.min(request.query['limit'] || 50, 50);
     docRef = docRef.limit(limit);
+    
+    // TODO: next/prevリンク組み立て
 
     docRef.get().then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
-        results.push(doc.data());
+        results["books"].push(doc.data());
       });
       response.status(200).send(results);
     })
@@ -65,20 +63,20 @@ exports.books = functions.https.onRequest((request, response) => {
 });
 
 
-async function doSearch(column, q) {
-  let results = await searchBooks(column, q);
+async function doSearch(bookTitle, personId) {
+  let results = await searchBooks(bookTitle, personId);
   return results;
 }
 
-function searchBooks(bookTitle, personName) {
+function searchBooks(bookTitle, personId) {
   return new Promise((resolve, reject) => {
     const results = [];
-    let bookRegex = new RegExp(bookTitle || ".*");
-    let personRegex = new RegExp(personName || ".*");
+    let bookRegex = new RegExp(bookTitle);
+    let personRegex = (personId) ? new RegExp("\""+ personId +"\"") : new RegExp(".*"); // ID指定があれば完全一致、なければ何でもヒットするように
     rs = fs.createReadStream('csv/books.csv');
     rs.pipe(csv({columns: true}))
       .on('data', (data) => {
-        if(data["作品名"].match(bookRegex) && data["姓名"].match(personRegex)){
+        if(data["作品名"].match(bookRegex) && data["人物ID"].match(personRegex)){
           results.push(data["作品ID"])
         }
     });
