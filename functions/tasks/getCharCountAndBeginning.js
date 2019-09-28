5784f0b96de8007ae8cf6be6df80590371ba08e7
tomomichi <tomomichi.onishi@gmail.com>
2019-09-28 14:11:28 +0900

    Fix csv transforming

--- functions/tasks/getCharCountAndBeginning.js
+++ functions/tasks/getCharCountAndBeginning.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const csv = require('csv-parse');
-const write = require('csv-write-stream')
+const csv = require('csv');
 const iconv = require('iconv-lite');
 const cheerio = require('cheerio');
-const through = require('through2')
 
 
 const parseFile = (filePath) => {
@@ -18,7 +16,7 @@ const parseFile = (filePath) => {
   return [charsCount, beginning.trim().slice(0,150)];
 }
 
-const transform = (data, enc, cb) => {
+const addCharsCountAndBeginning = (data) => {
   const url = data["XHTML/HTMLファイルURL"]
   const match = url.match(/(cards\/\d+\/files\/\d+_\d+\.html)/);
   if(match) {
@@ -26,13 +24,47 @@ const transform = (data, enc, cb) => {
     [charsCount, beginning] = parseFile(match[1]);
     data["文字数"] = charsCount;
     data["書き出し"] = beginning;
+    data["カテゴリ"] = categorize(charsCount);
+
   }
-  this.push(data)
-  cb()
+  return data;
 }
 
-fs.createReadStream('./tmp/csv/__all_books2.csv')
-  .pipe(csv({columns: true}))
-  .pipe(through.obj(transform))
-  .pipe(write())
-  .pipe(fs.createWriteStream('out.csv'))
+const addAccessCount = (data) => {
+  data["累計アクセス数"] = accessJson[data["作品ID"]] || 0;
+  return data;
+}
+
+const categorize = (charsCount) => {
+  let category = "";
+  if(charsCount > 0) { category = "flash"; }
+  if(charsCount > 2000) { category = "shortshort"; }
+  if(charsCount > 4000) { category = "short"; }
+  if(charsCount > 12000) { category = "novelette"; }
+  if(charsCount > 24000) { category = "novel"; }
+  return category;
+}
+
+const accessJsonFile = fs.readFileSync("./tmp/access.json", 'utf8');
+const accessJson = JSON.parse(accessJsonFile);
+
+const readStream = fs.createReadStream("./tmp/csv/__all_books2.csv"); // readStream is a read-only stream wit raw text content of the CSV file
+const writeStream = fs.createWriteStream("./tmp/output.csv"); // writeStream is a write-only stream to write on the disk
+const results = [];
+
+readStream.pipe(csv.parse({columns: true}))
+.on('data', (data) => {
+  console.log(data["作品名"])
+  data = addCharsCountAndBeginning(data);
+  data = addAccessCount(data);
+  results.push(data);
+})
+.on('error', (err) => {
+  console.log('error ------------', err);
+  reject();
+})
+.on('finish', () => {
+  console.log('all the csv strings parsed to objects -------------');
+})
+.pipe(csv.stringify({ header: true }))
+.pipe(writeStream);
